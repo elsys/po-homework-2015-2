beeb3323035c49701a4df8f0e931858273191756
mareto99 <kooke395@abv.bg>
2016-03-06 00:42:21 +0200

    Домашно

---
+++ V/01/19/task1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#define start 42
+
+long hash(char *word) {
+        long num = start;
+        for(int count = 0; count < strlen(word); count++) {
+            num += word[count]*(count+1);
+        }
+        return num;
+}
+
+int main() {
+    char word[200];
+    scanf("%s", word);
+    int sum = hash(word);
+    printf("%ld", sum);
+    return 0;
+}
