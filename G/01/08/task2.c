c55d947c86ad0afbf7b4059b57240cdc461f80e2
Georgi <Georgigt23@users.noreply.github.com>
2016-03-07 20:54:03 +0200

    Create task2.c

---
+++ G/01/08/task2.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct occurance_t{
+
+int reps;
+int hash;
+
+}occurance;
+
+int comparing(int array[],int counter);
+int hashing(char arr[]);
+
+int main(){
+
+    char str[200];
+    int i;
+    int hashes[3000] = {0};
+
+    for(i=0; i<3000 && strcmp(str,"vsmisal") != 0;i++){
+
+    scanf("%s",str);
+
+    hashes[i]=hashing(str);
+
+    }
+
+comparing(hashes,i);
+
+return 0;
+}
+
+int comparing(int array[], int counter){
+
+occurance var;
+var.reps=0;
+var.hash=0;
+int i,l=0,helper,helper2;
+
+for(i=0;i<counter;i++){
+l=i;
+helper=array[i];
+helper2=0;
+for(l=0;l<counter;l++){
+
+    if(helper==array[l]){
+        helper2++;
+        if(var.reps<helper2){var.reps=helper2;
+
+        var.hash=helper;
+        }
+    }
+
+}
+
+}
+printf("%d\n",var.reps);
+printf("%d",var.hash);
+}
+
+int hashing(char arr[]){
+
+int len=0,i,eq=42;
+
+len=strlen(arr);
+for(i=0;i<len;i++){
+
+    eq=eq+arr[i]*(i+1);
+
+    }
+
+return eq;
+}
+
