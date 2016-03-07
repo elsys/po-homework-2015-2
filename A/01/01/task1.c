1e1ff17d49a35c35c4a29636cf9b705bb2d84bb4
Yellobelly <alexstoyanof@gmail.com>
2016-03-07 22:58:13 +0200

    alexanderstoyano10aspimisewe

---
+++ A/01/01/task1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+long hash (char *word);
+int main ()
+{
+	char word[200];
+	fgets (word, 200 , stdin);
+	hash (word);
+	return 0;
+}
+long hash (char *word)
+{
+	int sum = 42,i , n = 0,temp;
+	n = strlen(word) - 1;
+	printf("%d\n", n );
+	for (i = 0; i < n; i++)
+	{
+		temp=word[i]*(i+1);
+		sum=sum +temp;
+	
+
+	}
+	printf("%d\n", sum );
+
+
+
+	return sum;
+}
