0d40113894854a311b1a28d5610aac13494f9e0f
DanielVladimirov <daniel.vvladimirov@gmail.com>
2016-03-08 00:13:44 +0200

    Daniel Vladimirov #8 10a

---
+++ A/01/08/task3.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+
+long hash(char* word);
+
+struct occurance_t{
+	long hash;
+	char collisions[4][201];
+};
+
+int main(){
+	char word[201];
+	struct occurance_t words[3000];
+	int i, j;
+
+	for(i = 0; i < 3000; i++){
+		words[i].hash = 0;
+		for(j = 0; j < 4; j++){
+			words[i].collisions[j][0] = 0;
+		}
+	}
+
+
+	while(1){
+		scanf("%s", word);
+		i = 0;
+
+		while(words[i].hash != 0 && i < 3000){
+			if(hash(word) == words[i].hash){
+
+				for(j = 0; words[i].collisions[j][0] != 0 && j < 4 && strcmp(word, words[i].collisions[j]) != 0; j++);
+				if(strcmp(word, words[i].collisions[j]) != 0)
+					strcpy(words[i].collisions[j], word);
+                break;
+			}
+			i++;
+		}
+		if(j == 3)
+            break;
+		if(words[i].hash == 0 && i < 3000){
+			words[i].hash = hash(word);
+			strcpy(words[i].collisions[0], word);
+		}
+	}
+
+	for(i = 0; i < 3000 && words[i].hash != 0; i++){
+		if(words[i].collisions[1][0] != 0){
+			printf("[%li]", words[i].hash);
+			for(j = 0; j < 4 && words[i].collisions[j][0] != 0; j++){
+				printf(" %s", words[i].collisions[j]);
+			}
+			printf("\n");
+		}
+	}
+
+	return 0;
+}
+
+long hash(char* word){
+	int i;
+	long hashed = 42;
+
+	for(i = 0; i < strlen(word); i++){
+		hashed += word[i]*(i+1);
+	}
+
+	return hashed;
+}
