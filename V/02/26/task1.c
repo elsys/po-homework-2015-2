6c1585157864d96e4b8b4858e5f2aabd5eeaa2cb
radito3 <radito3@mail.bg>
2016-03-14 19:47:50 +0200

    New Homework
    Рангел Иванов, 10 В, № 26, домашно № 2

---
+++ V/02/26/task1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void minmax(int*, int, int*, int*);
+
+int main(){
+    int max, size, min, i;
+    int* arr = (int*)malloc(sizeof(int)*400);
+    scanf("%d", &size);
+    for(i = 0; i < size; i++){
+        scanf("%d", &arr[i]);
+    }
+    minmax(arr, size, &min, &max);
+    printf("%d", min + max);
+    free(arr);
+    return 0;
+}
+
+void minmax(int *arr, int size, int *min, int *max){
+    int i;
+    *max = arr[0];
+    *min = arr[0];
+    for(i = 1; i < size; i++){
+        if(arr[i] > *max)
+            *max = arr[i];
+        if(arr[i] < *min)
+            *min = arr[i];
+    }
+}
