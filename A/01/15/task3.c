0245c783988b246a6fb70f118eb883084d79acb3
IvoTod <ivo.tod.todorov@gmail.com>
2016-03-15 15:15:21 +0200

    fix genadi so it can baca

--- A/01/15/task3.c
+++ A/01/15/task3.c
@@ -8,6 +8,8 @@ struct occurance_t{
 };
 
 long hash(char *word);
+int isRepeating(struct occurance_t *hashes, int hashPos, char *word);
+
 
 int main(){
 	char curWord[200];
@@ -16,28 +18,43 @@ int main(){
 	struct occurance_t temp;
 	int stop=0, hashCount=0, sort;
 	int i=0, l;
+	
 	scanf("%s", curWord);
 	curHash=hash(curWord);
+	
 	for(i=0; i<3000 && stop==0; i++){
+		
 		for(l=0; l<=i; l++){
+			
 			if(hashes[l].count==0){
 				hashes[l].hash=curHash;
 				strcpy(hashes[l].words[hashes[l].count], curWord);
 				hashes[l].count++;
+				
 				if(hashes[l].count==4){
 					stop=1;
 					break;
 				}
+				
 				hashCount++;
+				
 				break;
 			}
+			
 			if(hashes[l].hash==curHash){
+				
+				if(isRepeating(hashes, l, curWord)==1){
+					break;
+				}
+				
 				strcpy(hashes[l].words[hashes[l].count], curWord);
 				hashes[l].count++;
+				
 				if(hashes[l].count==4){
 					stop=1;
 					break;
 				}
+				
 				break;
 			}
 		}
@@ -50,21 +67,25 @@ int main(){
 		sort = 1;
 		for(i = 0; i<hashCount-1; i++) {
 			if (hashes[i].hash>hashes[i+1].hash) {
+				
 				temp = hashes[i];
 				hashes[i] = hashes[i+1];
                 hashes[i+1]=temp;
+				
 				sort = 0;
 			}
 		}
 	}
 	for(i=0; i<hashCount; i++){
 		if(hashes[i].count>1){
+			
 			printf("%ld ", hashes[i].hash);
+			
 			for(l=0; hashes[i].words[l][0]!=0 && l<4; l++){
 				printf("%s ", hashes[i].words[l]);
 			}
+			printf("\n");
 		}
-		printf("\n");
 	}
 	return 0;
 }
@@ -76,4 +97,17 @@ long hash(char *word){
 		res+=word[i-1] * i;
 	}
 	return res;
+}
+
+int isRepeating(struct occurance_t *hashes, int hashPos, char *word){
+	int i;
+	for(i=0; i<4; i++){
+		if(strcmp(word, hashes[hashPos].words[i]) == 0){
+			return 1;
+		}
+		if(word[0]=='\0'){
+			break;
+		}
+	}
+	return 0;
 }
