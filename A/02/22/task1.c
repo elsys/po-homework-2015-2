a577d457df57ac1efa5b4e25e7ac1c68fd836f44
nikolaystanishev <stanishev.nikolay@gmail.com>
2016-03-16 22:18:31 +0200

    Homework 02

--- A/02/22/task1.c
+++ A/02/22/task1.c
@@ -6,7 +6,7 @@ int main()
 {
 	int arr[500];
 	int size;
-	int *min, *max;
+	int min, max;
 	int n, i=0;
 	scanf("%d",&n);
 	size=n;
@@ -15,24 +15,24 @@ int main()
 		scanf("%d",&arr[i]);
 		i++;
 	}
-	min=arr;
-	max=arr;
-	minmax(arr, size, min, max);
-	printf("%d\n", *min+*max);
+	minmax(arr, size, &min, &max);
+	printf("%d", min+max);
 	return 0;
 }
 
 void minmax(int *arr, int size, int *min, int *max)
 {
+	*min=arr[0];
+	*max=arr[0];
 	for(int i=0; i!=size; i++)
 	{
-		if(*min>*(arr+i))
+		if(*min>arr[i])
 		{			
-			min=(arr+i);
+			*min=arr[i];
 		}
-		if(*max<*(arr+i))
+		if(*max<arr[i])
 		{
-			max=(arr+i);
+			*max=arr[i];
 		}
 	}
 }
