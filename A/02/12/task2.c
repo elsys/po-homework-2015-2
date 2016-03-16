20e9d60b48971dc52ab702b4739dae7010386872
Ivaza <iva.v.zahova@gmail.com>
2016-03-16 20:40:20 +0200

    working on hm
    task1.c soon

---
+++ A/02/12/task2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+char *find(char *, char);
+
+int main()
+{
+    char mas[400], y;
+    int i, j, k;
+    char *pos;
+
+	fgets(mas,400,stdin);
+	y=getchar();
+	k=strlen(mas);
+
+	pos=find(mas,y);
+	i=(int)(pos-mas);
+
+	if(i==k)
+    {
+        i=-1;
+    }
+	printf("%d",i);
+
+	return 0;
+}
+
+char *find(char *haystack, char needle)
+{
+	int i, len;
+
+	len=strlen(haystack);
+
+	for(i=0; i<len; i++)
+	{
+		if(*haystack==needle)
+        {
+			break;
+		}
+		else haystack++;
+	}
+	return haystack;
+}
---
+++ A/02/12/task3.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+
+int *hop(int *);
+int is_in_bound(int *, int, int *);
+
+int main()
+{
+    int arr[50],i,j, *p, hops, flag, num, lim;
+    char s[2000], *tok, sp[2]=" ";
+
+	fgets(s,2000,stdin);
+	i=strlen(s)-1;
+	s[i]='\0';
+
+    tok = strtok(s, sp);
+    num=0;
+    while(tok!=NULL)
+    {
+	  arr[num++]=atoi(tok);
+	  tok = strtok(NULL, sp);
+	};
+
+	flag=0;
+	p=arr;
+	hops=1;
+	do
+    {
+		p=hop(p);
+		lim=is_in_bound(arr,num,p);
+		if(!lim) hops++;
+	}while(*p!=0 && hops<50 && lim<1);
+
+	if(*p==0)
+    {
+        flag=1;
+    }
+	printf("%d\n%d",flag,hops);
+	return 0;
+}
+
+int *hop(int *elem)
+{
+	elem=elem+*elem;
+	return elem;
+}
+
+int is_in_bound(int *arr, int size, int *ptr)
+{
+	if(ptr>=arr && ptr<=(arr+size))
+    {
+        return 0;
+    }
+	else return 1;
+}
