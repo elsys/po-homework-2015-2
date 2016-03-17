d04410d391b5a5e4771dbe71116474ce6ad4bb89
ivanganchev <ivan_ganchev45@abv.bg>
2016-03-17 22:33:20 +0200

    Ivan Ganchev Homework 02 and fixed hw1 task1 and task3nategameushkite 13 aaaopaopacakcak

--- A/01/13/task1.c
+++ A/01/13/task1.c
@@ -3,10 +3,13 @@
 int long_hash(char *word);
 int main()
 {
-    int sum = 42;
+    int sum = 42, i = 0;
     int result;
     char str[200];
-
+    for(i = 0; i < 200;i++)
+	{
+		str[i] = 0;
+	}
     scanf("%s", str);
 
     result = long_hash(str);
