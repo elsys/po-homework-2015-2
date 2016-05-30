2e6e4524ec9af93a04d5e9f788c95ae6c0b1e590
David-Nicollas <david.nicolas06.99@Gmail.com>
2016-05-30 21:23:03 +0300

    task1.c

--- G/01/09/task1.c
+++ G/01/09/task1.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-long hash(char *word){
-	long res = 42;	
-	int i=0;
-	int multiplier = 1;
-	char duma[200];
-	int lenght = strlen(duma); 
-	strcpy(duma, word);
-	do{
-		res = res+(duma[i]*multiplier);
-		i++;
-		multiplier++;
-	}while(i<=lenght);
-	
-	return res;
+int longhash(char* word){
+	int result = 42;
+	int i;
+	for(i = 0; i <= strlen(word) - 1; i++){
+		printf("word: %c\n", word[i]);
+		result += word[i] * (i+1);
+	}
+
+	return result;
 }
 
 int main(){
 
-	char word[200];
-	scanf("%s", &word);
-	printf("%ld",hash(word));
+	char *str = malloc(200 * sizeof(char));
 
-return 0;
+	int i;
+	printf("Enter a string: ");
+	fgets(str, 200, stdin);
+
+	while(strlen(str) >= 200){
+		printf("Enter a string: ");
+		fgets(str, 200, stdin);
+	}
+
+	i = strlen(str)-1;
+	if( str[ i ] == '\n') 
+	  str[i] = '\0';
+
+	printf("hash = %d", longhash(str));
+
+	return 0;
 }
